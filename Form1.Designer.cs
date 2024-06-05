5374d23a6d037a7e42e2c2dc2425731ce8e33be9
Rolando Goloya Jr <rgoloyajr@gmail.com>
2024-06-05 21:20:07 +0800

    my Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace BMI_Automated_Computing_cs4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label_height = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label_age = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Cyan;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(402, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BMI Automated Calculation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Weight in (kg) kilogram: ";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeight.Location = new System.Drawing.Point(237, 180);
+            this.txtHeight.Multiline = true;
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(85, 37);
+            this.txtHeight.TabIndex = 3;
+            // 
+            // label_height
+            // 
+            this.label_height.AutoSize = true;
+            this.label_height.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_height.Location = new System.Drawing.Point(12, 182);
+            this.label_height.Name = "label_height";
+            this.label_height.Size = new System.Drawing.Size(225, 25);
+            this.label_height.TabIndex = 4;
+            this.label_height.Text = "Height in (m) Meter: ";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnReset.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnReset.Location = new System.Drawing.Point(177, 358);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(145, 49);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label_age
+            // 
+            this.label_age.AutoSize = true;
+            this.label_age.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_age.Location = new System.Drawing.Point(12, 134);
+            this.label_age.Name = "label_age";
+            this.label_age.Size = new System.Drawing.Size(54, 25);
+            this.label_age.TabIndex = 10;
+            this.label_age.Text = "Age:";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(72, 132);
+            this.txtAge.Multiline = true;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(63, 37);
+            this.txtAge.TabIndex = 11;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCalculate.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnCalculate.Location = new System.Drawing.Point(17, 359);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(145, 49);
+            this.btnCalculate.TabIndex = 12;
+            this.btnCalculate.Text = "Calculate BMI";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnExit.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnExit.Location = new System.Drawing.Point(337, 357);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(145, 51);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtResult.Location = new System.Drawing.Point(17, 266);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(465, 83);
+            this.txtResult.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 25);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Name:";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWeight.Location = new System.Drawing.Point(282, 223);
+            this.txtWeight.Multiline = true;
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(85, 37);
+            this.txtWeight.TabIndex = 17;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(91, 89);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(288, 37);
+            this.txtName.TabIndex = 18;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(497, 427);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label_age);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label_height);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Name = "Form1";
+            this.Text = "CS4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label_height;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label_age;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Automated_Computing_cs4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myObj.Name = txtName.Text;
+            Console.WriteLine(myObj.Name);
+            inhe.greetings();
+            poly.give_advice();
+            inhe.give_thanks();
+        }
+
+
+        // Encapsulation / the value is access through form1 load
+        class Person 
+        {
+            private string name; // access modifier private
+            public string Name   // property
+            {
+                get { return name; } //getmethod
+                set { name = value; } //setmethod
+            }
+        }
+        Person myObj = new Person(); // Object
+
+
+        // Inheritance & Polymorphism
+        public class Reminder //Parent Class
+        {
+            public Reminder()
+            {
+
+            }
+
+            public void greetings()
+            {
+                Console.WriteLine("Magandang Umaga Kapatid! \n");
+            }
+
+            public virtual void give_advice() //using virtual
+            {
+                Console.WriteLine("Your so beautiful!");
+            }
+            public  void give_thanks()
+            {
+                Console.WriteLine("Maraming Salamat po! \n \n \n");
+            }
+        }
+        public class Poly : Reminder // this is a example of polymorphism using Poly as child class
+        {
+            public override void give_advice() //using override
+            {
+                Console.WriteLine("     Mahalagang Paalala! Ang pagkain ng vegetable at frutas ay makakatulong upang magkaroon ng malusog na pangangatawan. " +
+                    "Sabayan mo pa ito ng ehersisyo para sa katibayan ng buto-buto \n");
+            }
+        }
+        public class Inhe : Reminder // this is a example of inheritance using Inhe as child class
+        {
+           
+        }
+        Poly poly = new Poly(); // Object for polymorphism
+        Inhe inhe = new Inhe(); // Object for inheritance
+
+
+        //Abstraction
+        abstract class Classifieds
+        {
+            public abstract void value();
+        }
+        class Normal : Classifieds //Normal is a child class
+        {
+            public override void value()
+            {
+                Console.WriteLine("Reminder:   Congrats! you made it, Maintain your health and be more productive fighting \n");
+            }
+        }
+        class Underwieght : Classifieds //Underwieght is a child class
+        {
+            public override void value()
+            {
+                Console.WriteLine("Reminder:   Kindly, do eating healthy foods with perfect exercise for you to become healthy \n");
+            }
+        }
+        class Overwieght : Classifieds //Overwieght is a child class
+        {
+            public override void value()
+            {
+                Console.WriteLine("Reminder:   You Can Do It, Exercise for the better ..Goodluck \n");
+            }
+        }
+        class Obese : Classifieds //Obese is a child class
+        {
+            public override void value()
+            {
+                Console.WriteLine("Reminder:   Get out the bet and Go to the Gym King \n");
+            }
+        }
+        Classifieds normal = new Normal(); // Object
+        Classifieds underwieght = new Underwieght(); // Object
+        Classifieds overwieght = new Overwieght(); // Object
+        Classifieds obese = new Obese(); // Object
+
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtAge.Text = "";
+            txtHeight.Text = "";
+            txtWeight.Text = "";
+            txtResult.Text = "";
+        }
+
+        public void btnCalculate_Click(object sender, EventArgs e)
+        {   
+            //using if else statement to validate the value of textbox
+            if(txtName.Text != " " & txtAge.Text != "" & txtHeight.Text != "" )
+            {
+                double height = (Convert.ToDouble(txtHeight.Text)); // fields
+                double weight = (Convert.ToDouble(txtWeight.Text)); // fields
+
+                Classification myObj = new Classification(" Classification: Underweigth", " Classification: Normalweight", " Classification: Overweight", " Classification: Obese");// Create an object of the Classification Class (this will call the constructor)
+                
+                txtResult.Text += "BMI: ";
+                txtResult.Text += Calculate(height, weight);
+
+                if (Calculate(height, weight) <= 18)
+                {
+                    txtResult.Text += "\n" + myObj.Uw;
+                    underwieght.value();
+                }
+                else if (Calculate(height, weight) <= 24.9)
+                {
+                    txtResult.Text += "\n" + myObj.N;
+                    normal.value();
+                }
+                else if (Calculate(height, weight) <= 30)
+                {
+                    txtResult.Text += "\n" + myObj.Ov;
+                    overwieght.value();
+                }
+                else if(Calculate(height, weight) >= 31)
+                {
+                    txtResult.Text += "\n" + myObj.Ob;
+                    obese.value();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Fill up the Form", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // method with return value
+        public double Calculate(double height, double weight) 
+        {
+            double result = weight / (height * height);
+            return result;
+        }
+
+        // class
+        class Classification 
+        {
+            // class members using "public" access modifiers
+            public String Uw; // class fields
+            public String N; // class fields
+            public String Ov; // class fields
+            public String Ob;
+
+            // Create a class constructor with multiple parameters
+            public Classification(String setUw, String setN, String setOv, String setOb)
+            {
+                Uw = setUw;
+                N = setN;
+                Ov = setOv;
+                Ob = setOb;
+            }
+        }       
+    }
+}
+
